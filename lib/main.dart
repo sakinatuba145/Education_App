7e18ec976849c23764be336636a48a01607d381b
muskanmomini <muskanmomini01@gmail.com>
2026-06-25 23:05:55 +0430

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:education_app/courses/course_screen.dart';
+import 'theme_provider.dart';
 import 'package:education_app/quiz/quiz_model.dart';
 import 'package:education_app/quiz/quiz_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,15 +9,12 @@ import 'features/forgot_password.dart';
 import 'firebase_options.dart';
 
 import 'courses/course_bloc.dart';
-import 'theme_provider.dart';
 
 import 'core/constants/theme.dart';
 import 'core/helpers/shared_preferences_helper.dart';
-import 'features/welcome_screen.dart';
 import 'features/login_screen.dart';
 import 'features/register_screen.dart';
 import 'dashboard/dashboard_screen.dart';
-import 'teacher/screens/teacher_dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -57,9 +54,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       themeMode:themeProvider.themeMode,
       routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegisterScreen.id: (context) => RegisterScreen(),
+        LoginScreen.id: (context) => LoginScreen(toggleTheme: () {  },),
+        RegisterScreen.id: (context) => RegisterScreen(Theme: () {  },),
         ForgotPasswordScreen.id: (context) => ForgotPasswordScreen(),
         DashboardScreen.id: (context) => DashboardScreen(),
         TeacherDashboardScreen.id: (context) => TeacherDashboardScreen(),
@@ -67,7 +63,7 @@ class MyApp extends StatelessWidget {
         QuizScreen.id: (context) => QuizScreen(exam: ModalRoute.of(context)!. settings.arguments as ExamModel,),
       },
 
-      initialRoute: WelcomeScreen.id,
+      initialRoute: RegisterScreen.id,
     );
   }
 }

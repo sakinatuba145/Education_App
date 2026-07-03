721700a5e9e8de4db670f20599455ebe482648cd
SakinaKarimi <sakinatuba145@gmail.com>
2026-07-03 20:16:32 +0430

    some item added in dashboard screen

--- lib/core/constants/theme.dart
+++ lib/core/constants/theme.dart
@@ -254,9 +254,10 @@ class AppTheme {
 }
 
  class ThemeColors {
-   static const primary =git);
+   static const primary = Color(0xFFFFA726);
    static const secondary = Color(0xFFFFCC80);
    static const background = Color(0xFFFFF3E0);
+
 
 
    static const gradient1 = Color(0xFFFFF8F0);
